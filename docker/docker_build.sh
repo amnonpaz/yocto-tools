887744daa3ab1b42cb2af9855ffc57c91bbaabc2
Amnon Paz <pazamnon@gmail.com>
2024-10-02 15:34:52 +0300

    Update Dockerfile to use ubuntu:22.04

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION=${1}
-if [ -z "$VERSION" ] ; then
-    echo "Please provide version"
+IMAGE_NAME=${1}
+if [ -z "$IMAGE_NAME" ] ; then
+    echo "Please provide image name"
     exit 1
 fi
 
-docker build --build-arg "HOST_UID=$(id -u)"  --build-arg "HOST_GID=$(id -g)" --tag yocto/builder:${VERSION} .
+docker build --build-arg USERNAME=$(id -nu) --build-arg PASSWORD=$(id -nu) --build-arg HOST_UID=$(id -u) --build-arg HOST_GID=$(id -g) --tag ${IMAGE_NAME} .
